5f32658b59916697971b4657d05d2907f496b530
akarac <akarac@softserveinc.com>
2020-12-16 15:28:28 +0200

    Lesson 4

--- src/Components/Lessons/Lesson4/index.js
+++ src/Components/Lessons/Lesson4/index.js
@@ -1,12 +1,75 @@
-import React, { useState, useCallback } from "react";
+import React, {
+  useRef,
+  useState,
+  useMemo,
+  useCallback,
+  useEffect,
+} from "react";
 
 import "./index.css";
 
+const PseudoInput = props => {
+  return <input className={"ant-input"} {...props} />;
+};
+
+const PseudoCheckbox = props => {
+  return <input className={"ant-input"} {...props} type={"checkbox"} />;
+};
+
+// we want to have { name, checked } structure as value
+const CombinedComponent = ({ value, onChange }) => {
+  const isInitialized = useRef(false);
+  const inputValue = useMemo(() => value?.name ?? "", [value]);
+  const checkboxValue = useMemo(() => value?.checked ?? false, [value]);
+
+  const handleInputChange = useCallback(
+    e => {
+      const {
+        target: { value: name },
+      } = e;
+      const checked = name === "Hello World" ? true : checkboxValue;
+      onChange?.({ ...value, name, checked });
+    },
+    [value, onChange, checkboxValue]
+  );
+  const handleCheckboxChange = useCallback(
+    e => onChange?.({ ...value, checked: e.target.checked }),
+    [value, onChange]
+  );
+
+  // useEffect(() => {
+  //   if (isInitialized.current === false) {
+  //     return (isInitialized.current = true);
+  //   }
+
+  //   if (inputValue === "Hello World" && !checkboxValue) {
+  //     onChange?.({ ...value, checked: true });
+  //   }
+  // }, [inputValue]);
+
+  return (
+    <div>
+      <div className={"combined-component"}>
+        <PseudoInput value={inputValue} onChange={handleInputChange} />
+        <PseudoCheckbox
+          checked={checkboxValue}
+          onChange={handleCheckboxChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+const StateFullCombinedComponent = () => {
+  const [value, onChange] = useState({ name: "Hello World", checked: false });
+
+  return <CombinedComponent value={value} onChange={onChange} />;
+};
 
 const Main = () => {
   return (
     <div className={"lesson4 main"}>
-      Guten tag
+      <StateFullCombinedComponent />
     </div>
   );
 };

d19e9a16383c057c5ac5ce7aaf2b6922024eb07a
oleg.gerus <gerus.oleg@gmail.com>
2021-01-16 21:24:31 +0200

    updated layout + routing

--- src/App.js
+++ src/App.js
@@ -1,65 +1,26 @@
 import React, { useState } from "react";
 import "antd/dist/antd.css";
 import "./index.css";
-import { Layout, Menu, Breadcrumb } from "antd";
-import {
-  DesktopOutlined,
-  PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { Layout } from "antd";
 
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+import LeftMenu from "./Layout/LeftMenu";
+import AppContent from "./Layout/Content";
+const { Header, Footer } = Layout;
 
 const BaseLayout = () => {
-  const [collapsed, setCollapsed] = useState(false);
-
-  const onCollapse = (collapsed) => {
-    console.log(collapsed);
-    setCollapsed(collapsed);
+  const [displayItem, updateDisplayItem] = useState(1);
+  const handleMenuClick = (menu) => {
+    updateDisplayItem(menu.key);
   };
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-        <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
-            Oleh
-          </Menu.Item>
-          <Menu.Item key="2" icon={<DesktopOutlined />}>
-            Anton
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-            <Menu.Item key="3">Lesson 1</Menu.Item>
-            <Menu.Item key="4">Lesson 2</Menu.Item>
-            <Menu.Item key="5">Lesson 3</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9" icon={<FileOutlined />}>
-            Files
-          </Menu.Item>
-        </Menu>
-      </Sider>
+      <LeftMenu handleClick={handleMenuClick} />
       <Layout className="site-layout">
         <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: "0 16px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div
-            className="site-layout-background"
-            style={{ padding: 24, minHeight: 360 }}
-          >
-            Bill is a cat.
-          </div>
-        </Content>
+
+        <AppContent itemToDisplay={displayItem} />
+
         <Footer style={{ textAlign: "center" }}>
           Ant Design ©2018 Created by Ant UED
         </Footer>
---
+++ src/Layout/Content.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./../index.css";
+import { Layout, Breadcrumb } from "antd";
+
+import Anton from "../Components/PersonalInfo/anton";
+import Oleh from "../Components/PersonalInfo/oleh";
+import Lesson1 from "../Components/Lessons/Lesson1";
+import Lesson2 from "../Components/Lessons/Lesson2";
+import Lesson3 from "../Components/Lessons/Lesson3";
+import Lesson4 from "../Components/Lessons/Lesson4";
+import Lesson5 from "../Components/Lessons/Lesson5";
+
+const { Content } = Layout;
+
+const AppContent = ({ itemToDisplay }) => {  
+  const components = {
+    1: (
+      <div>
+        <Oleh />
+      </div>
+    ),
+    2: (
+      <div>
+        <Anton />
+      </div>
+    ),
+    3: (
+      <div>
+        <Lesson1 />
+      </div>
+    ),
+    4: (
+      <div>
+        <Lesson2 />
+      </div>
+    ),
+    5: (
+      <div>
+        <Lesson3 />
+      </div>
+    ),
+    6: (
+      <div>
+        <Lesson4 />
+      </div>
+    ),
+    7: (
+      <div>
+        <Lesson5 />
+      </div>
+    ),
+    // 8: <div><Lesson6/></div>,
+    // 9: <div><Lesson7/></div>,
+    19: <div>Empty</div>,
+  };
+
+  return (
+    <>
+      <Content style={{ margin: "0 16px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>User</Breadcrumb.Item>
+          <Breadcrumb.Item>Oleh</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          className="site-layout-background"
+          style={{ padding: 24, minHeight: 360 }}
+        >
+          {components[itemToDisplay]}
+        </div>
+      </Content>
+    </>
+  );
+};
+
+export default React.memo(AppContent);

48c73f1ed83ef8ffbca73300958dc55c4e7b022b
akarac <akarac@softserveinc.com>
2021-01-26 14:44:24 +0200

    lesson 6

--- src/Components/Lessons/Lesson6/index.js
+++ src/Components/Lessons/Lesson6/index.js
@@ -3,7 +3,13 @@ import { connect } from "react-redux";
 
 import ReduxProvider from "./setupRedux";
 
-const ReduxConsumerComponentTemplate = ({ dispatch, counter }) => {
+const ReduxConsumerComponentTemplate = ({
+  dispatch,
+  onClick,
+  onDoubleClick,
+  onCheck,
+  counter,
+}) => {
   //   dispatch({ type: "a", someotherProp: 1 });
   //   dispatch({ type: 1 });
   //   dispatch({ type: 1, payload: { payloadKey1: "22" } });
@@ -11,16 +17,37 @@ const ReduxConsumerComponentTemplate = ({ dispatch, counter }) => {
   return (
     <div>
       ReduxConsumerComponentTemplate {counter}
-      <button onClick={() => dispatch({ type: "a", someotherProp: 1 })}>
-        Add one
-      </button>
+      <button onClick={onClick}>Add one</button>
+      <button onClick={onCheck}>Add two</button>
     </div>
   );
 };
+
+const sendData = async dispatch => {
+  let url =
+    "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits";
+  let res = await (await fetch(url)).json();
+
+  dispatch({ type: "github_counter", payload: res.length, someotherProp: 1 });
+};
+
 const ReduxConsumerComponent = connect(
   state => ({ counter: state.a }),
   dispatch => ({
     dispatch,
+    onClick: () => dispatch({ type: "a", someotherProp: 1 }),
+    onDoubleClick: () => dispatch(sendData),
+    onCheck: async () => {
+      let url =
+        "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits";
+      let res = await (await fetch(url)).json();
+
+      dispatch({
+        type: "github_counter",
+        payload: res.length,
+        someotherProp: 1,
+      });
+    },
   }),
   null,
   {}
--- src/Components/Lessons/Lesson6/setupRedux.js
+++ src/Components/Lessons/Lesson6/setupRedux.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 
 const reducer = (state = { a: "" }, action) => {
   console.log(action);
@@ -9,9 +10,15 @@ const reducer = (state = { a: "" }, action) => {
       ...state,
       a: +state.a + 1,
     };
+  if (action.type === "github_counter") {
+    return {
+      ...state,
+      a: +action.payload,
+    };
+  }
   return state;
 };
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const Main = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
